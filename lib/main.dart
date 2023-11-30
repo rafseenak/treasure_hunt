9223019ca8170ed8097746080d141bcc50f04ab2
Rafseena K <rafseenak2001@gmail.com>
2023-12-01 03:27:23 +0530

    80

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/first_screen.dart';
 import 'package:frontend/result.dart';
-import 'package:splashscreen/splashscreen.dart';
 import 'controller/qr_controller.dart';
 
 main() {
@@ -23,25 +22,6 @@ class MyApp extends StatelessWidget {
         'rslt': (context) => const CenteredText(),
         'rslt2': (context) => const CenteredText2(),
       },
-    );
-  }
-}
-
-class Splash2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return SplashScreen(
-      seconds: 6,
-      navigateAfterSeconds: new SecondScreen(),
-      title: new Text(
-        'GeeksForGeeks',
-        textScaleFactor: 2,
-      ),
-      image: new Image.network(
-          'https://www.geeksforgeeks.org/wp-content/uploads/gfg_200X200.png'),
-      loadingText: Text("Loading"),
-      photoSize: 100.0,
-      loaderColor: Colors.blue,
     );
   }
 }
